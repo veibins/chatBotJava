a37365984962b74ece24208e3edb9eae4bc2e5d8
ChristianW <81246807+veibins@users.noreply.github.com>
2022-01-04 22:51:40 +0200

    While loop attachment
    Loop that doesn't allow for code to execute further until valid integer is entered.

--- src/com/kvstarter/Main.java
+++ src/com/kvstarter/Main.java
@@ -5,44 +5,50 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        
+        String choiceOne = "Java";
+        String choiceTwo = "C++";
+        String choiceThree = "WEB/JS/HTML";
+        String statementOne = ("Tiekamies mācībās! ;)");
+        int choiceCodeLang = 0;
+
+        /*defining variables*/
 
         Scanner scanInput = new Scanner(System.in);
 
         System.out.println("Sveiks! Tu šobrīd atrodies \"SuperDuper\" programmēšanas skolas mājaslapā!");
         System.out.print("Lai turpinātu, lūdzu ievadi savu vārdu un spied Enter: ");
 
-        String name = scanInput.nextLine();
+        String input = scanInput.nextLine();
 
-        String choiceOne = "Java";
-        String choiceTwo = "C++";
-        String choiceThree = "WEB/JS/HTML";
-
-        System.out.println("Laipni lūgts/a \"SuperDuper\" skolā, " + name + "!");
+        System.out.println("Laipni lūgts/a \"SuperDuper\" skolā, " + input + "!");
         System.out.println("Lūdzu ieraksti, kādu programmēšanas valodu vēlies apgūt un spied Enter: (raksti \"1\", ja vēlies apgūt " + choiceOne +
                 " \"2\", ja vēlies apgūt " + choiceTwo + " \"3\", ja vēlies apgūt " + choiceThree + ")");
 
-        int choiceCodeLang = scanInput.nextInt();
+        /*while loop to enter a valid integer for code to continue executing further*/
 
-        if (choiceCodeLang == 1) {
-            System.out.println("Paldies! Tu izvēlējies macīties " + choiceOne + ".");
+        while (choiceCodeLang > 3 || choiceCodeLang < 1) {
+            if (scanInput.hasNextInt()) {
+                choiceCodeLang = scanInput.nextInt();
+            } else {
+                scanInput.next();
+            }
+            if (choiceCodeLang > 3 || choiceCodeLang < 1) {
+                System.out.println("Atvaino, bet šāda apmācību programma šobrīd vēl neeksistē.");
+                System.out.println("Lūdzu ieraksti, kādu programmēšanas valodu vēlies apgūt un spied Enter: (raksti \"1\", ja vēlies apgūt " + choiceOne +
+                        " \"2\", ja vēlies apgūt " + choiceTwo + " \"3\", ja vēlies apgūt " + choiceThree + ")");
+            } else {
+                if (choiceCodeLang == 1) {
+                    System.out.println("Paldies! Tu izvēlējies macīties " + choiceOne + ". " + statementOne);
+                }
+                if (choiceCodeLang == 2) {
+                    System.out.println("Paldies! Tu izvēlējies macīties " + choiceTwo + ". " + statementOne);
+                }
+                if (choiceCodeLang == 3) {
+                    System.out.println("Paldies! Tu izvēlējies macīties " + choiceThree + ". " + statementOne);
+                }
+            }
         }
-        if (choiceCodeLang == 2) {
-            System.out.println("Paldies! Tu izvēlējies macīties " + choiceTwo + ".");
-        }
-        if (choiceCodeLang == 3){
-            System.out.println("Paldies! Tu izvēlējies macīties " + choiceThree + ".");
-        }
-
-        System.out.println("Tiekamies mācībās! ;)");
-
         scanInput.close();
-
-
-
-
-
-
-
-
     }
 }
